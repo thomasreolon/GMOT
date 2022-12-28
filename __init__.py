3c8b38239a5146e648817402b640d16f2581b931
thomas <thomas.reolon.it@gmail.com>
2022-12-29 00:29:12 +0900

    add gaussian embedd

--- __init__.py
+++ __init__.py
@@ -7,64 +7,76 @@ import math
 import torch
 
 
-#### get pos 1
-def sine1(H=5, W=10, num_feat=64, speed=1, strength=.1):
-    emb = torch.arange(num_feat).view(1,1,num_feat).expand(H,W,-1).clone() / num_feat * 3.14*10
 
-    embx = torch.sin(emb + speed*torch.arange(W).view(1,-1,1)/(W/3.15)) * strength
-    emby = torch.sin(emb + speed*torch.arange(H).view(-1,1,1)/(H/3.15)) * strength
+# #### get pos 1
+# def sine1(H=2, W=2, num_feat=8, smooth=50):
+
+#     feat_range = torch.arange(num_feat).view(1,1,num_feat).expand(H,W,-1).clone() / num_feat +0.5/num_feat
+#     feat_w = torch.arange(W).view(1,W,1).expand(H,-1,num_feat).clone() / H +0.5/H
+#     feat_h = torch.arange(H).view(H,1,1).expand(-1,W,num_feat).clone() / W +0.5/W
     
-    a = torch.arange(num_feat).view(1,-1) / num_feat
-    h = torch.arange(H).view(-1,1) / H
-    w = torch.arange(W).view(-1,1) / W
-    h = torch.exp(- ((a-h) **2))
-    w = torch.exp(- ((a-w) **2))
+#     w_similarity = 0.01 + torch.exp(-(feat_range-feat_w)**2 *smooth)
+#     h_similarity = 0.01 + torch.exp(-(feat_range-feat_h)**2 *smooth)
+
+#     features = torch.cat((w_similarity*1,  h_similarity*1), dim=2)
+
+#     print(features.max(), features.std(), features.mean(), features.min() )
+
+#     return features.numpy()
+#     # a = torch.arange(num_feat).view(1,-1) / num_feat
+#     # h = torch.arange(H).view(-1,1) / H
+#     # w = torch.arange(W).view(-1,1) / W
+#     # h = torch.exp(- ((a-h) **2))
+#     # w = torch.exp(- ((a-w) **2))
 
 
 
-    return emb.numpy()
+#     # return emb.numpy()
 
-def sineold(img_size=100, num_feat=64):
-    mask = torch.zeros(1,img_size,img_size).bool()
-    not_mask = ~mask
+# def sineold(img_size=500, num_feat=64):
+#     mask = torch.zeros(1,img_size,img_size).bool()
+#     not_mask = ~mask
 
-    y_embed = not_mask.cumsum(1, dtype=torch.float32)
-    x_embed = not_mask.cumsum(2, dtype=torch.float32)
+#     y_embed = not_mask.cumsum(1, dtype=torch.float32)
+#     x_embed = not_mask.cumsum(2, dtype=torch.float32)
 
-    dim_t = torch.arange(num_feat, dtype=torch.float32)
-    dim_t = 10000 ** (2 * (dim_t // 2) / num_feat)
+#     dim_t = torch.arange(num_feat, dtype=torch.float32)
+#     dim_t = 10000 ** (2 * (dim_t // 2) / num_feat)
 
-    pos_x = x_embed[:, :, :, None] / dim_t
-    pos_y = y_embed[:, :, :, None] / dim_t
-    pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
-    pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
-    pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
-    return pos[0].permute(1,2,0).numpy()
+#     pos_x = x_embed[:, :, :, None] / dim_t
+#     pos_y = y_embed[:, :, :, None] / dim_t
+#     pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
+#     pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
+#     pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
+#     return pos[0].permute(1,2,0).numpy()
 
 
-images = [
-    ('old', sineold()),
-    # ('s1', sine1()),
-    # ('s1_5', sine1(speed=5)),
-    # ('s1_9', sine1(strength=.4)),
-    # ('s1_9', sine1(speed=5, strength=.4)),
-]
+# images = [
+#     ('old', sineold(4,8)),
+#     # ('s1', sine1(500,500,16)),
+#     # ('s2', sine1(66,66,64)),
+#     # ('s1_5', sine1(speed=5)),
+#     # ('s1_9', sine1(strength=.4)),
+#     # ('s1_9', sine1(speed=5, strength=.4)),
+# ]
 
-for name, img in images:
+# for name, img in images:
 
-    center = img[50,50]
-    tri_right = (img[50,50] + img[75,75] + img[25,75]) / 3
-    tri_left = (img[25,25] + img[75,25])
-    topright = (img[0,0])
+#     h,w,_ = img.shape
+#     tri_right = (img[h//2,w//2] + img[h*2//3,w*2//3] + img[h//3,w*2//3]) / 3
 
-    for R, tit in zip([center, tri_right, tri_left, topright], ('cent', 'right', 'left', 'topleft')):
-        tmp=(img[:,:,None,:] @ R.reshape(1,1,-1,1))[:,:,0]
-        # tmp = np.exp(tmp)
-        tmp = (tmp-tmp.min()) / (tmp.max()-tmp.min())
+#     tri_left = (img[h//4,w//4] + img[h*3//4,w//4])
+#     topright = (img[0,0])
+#     center = img[h//2,w//2]
+#     toprow = (img[h*2//3,w//4]+ img[h//4,w*3//4])
 
-        cv2.imshow(name+tit, tmp)
-cv2.waitKey()
-exit()
+#     for R, tit in zip([center, tri_right, tri_left, topright, toprow], ('cent', 'right', 'left', 'topleft', 'diag')):
+#         tmp=(img[:,:,None,:] @ R.reshape(1,1,-1,1))[:,:,0]
+#         # tmp = np.exp(tmp)
+#         tmp = (tmp-tmp.min()) / (tmp.max()-tmp.min())
+
+#         cv2.imshow(name+tit, tmp)
+# cv2.waitKey()
 
 if __name__=='__main__':
     import torch
@@ -96,3 +108,16 @@ if __name__=='__main__':
     ### remember that:     if solution = 0.6,0.8 ---> real: 0.6 /orig_size *(origsize+PREpad)
 
     print()
+
+    print('>> testing gaussin embedding')
+    from models.positionembedd.gaussian_embedd import GaussianEmbedder
+    cl = GaussianEmbedder(32)
+
+    fmap=cl.get_fmap_pos(torch.rand(1,2,100,100))[0].permute(1,2,0)
+    queries = cl.get_q_pos(torch.tensor([[0.9,0.99],[0.2,0.8]]))
+
+    img = fmap @ queries.sum(0).view(-1,1)
+    img = (img-img.min()) / (img.max()-img.min())
+    cv2.imshow('similarity of queries wrt image', img.numpy())
+    cv2.waitKey()
+
---
+++ models/positionembedd/gaussian_embedd.py
@@ -0,0 +1,37 @@
+import torch
+
+
+
+class GaussianEmbedder(torch.nn.Module):
+
+    def __init__(self, size, strength=50):
+        super().__init__()
+        self.size = size
+        self.strength = strength
+
+
+    def get_fmap_pos(self, feat_map):
+        B,_,H,W = feat_map.shape
+        size = self.size //2
+
+        size_range = torch.arange(size).view(1,1,size).expand(H,W,-1).clone() / size +0.5/size
+        h_embedd = torch.arange(H).view(H,1,1).expand(-1,W,size).clone() / W +0.5/W
+        h_embedd = 0.01 + torch.exp(-(size_range-h_embedd)**2 *self.strength)
+        
+        w_embedd = torch.arange(W).view(1,W,1).expand(H,-1,size).clone() / H +0.5/H
+        w_embedd = 0.01 + torch.exp(-(size_range-w_embedd)**2 *self.strength)
+
+        embedd = torch.cat((h_embedd, w_embedd), dim=2).to(feat_map.device)
+        return embedd.permute(2,0,1).unsqueeze(0).expand(B,-1,-1,-1)
+
+    def get_q_pos(self, ref_pts):
+        size = self.size //2
+        h_embedd = (torch.arange(size)/ size +0.5/size).view(1,-1)
+        h_embedd = h_embedd - ref_pts[:,0].unsqueeze(1)
+        h_embedd = 0.01 + torch.exp(-h_embedd**2 *self.strength)
+
+        w_embedd = (torch.arange(size)/ size +0.5/size).view(1,-1)
+        w_embedd = w_embedd - ref_pts[:,1].unsqueeze(1)
+        w_embedd = 0.01 + torch.exp(-w_embedd**2 *self.strength)
+
+        return torch.cat((h_embedd, w_embedd), dim=1).to(ref_pts.device)
