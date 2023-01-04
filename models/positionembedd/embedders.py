60074356093c3fd4c0c307abffe3c3c6b250a78a
thomas <thomas.reolon.it@gmail.com>
2023-01-04 23:16:26 +0900

    add small embedder

--- models/positionembedd/embedders.py
+++ models/positionembedd/embedders.py
@@ -2,8 +2,24 @@ import torch
 
 
 
-class concat_embedd(torch.nn.Module):
+class ConcatPos(torch.nn.Module):
 
-    def __init__(self, embedd_size):
-        pass
+    def __init__(self, embedd_size, pos_embedd_size=32):
+        super().__init__()
+        self.linear = torch.nn.Linear(embedd_size, embedd_size-pos_embedd_size)
+    
+    def forward(self, x, pos):
+        x = x.linear(x)
+        return torch.cat((x,pos), dim=-1)
+
+
+
+class SumPos(torch.nn.Module):
+    def __init__(self, embedd_size, pos_embedd_size=32):
+        super().__init__()
+    
+    def forward(self, x, pos):
+        return x if pos is None else x+pos
+
+
 
